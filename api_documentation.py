069bee6597e4bc69e0ffb9353a448aafef71db48
dxvsh <vashishtdakksh@gmail.com>
2023-11-24 14:56:44 +0530

    update the base_url in the api documentation

--- api_documentation.py
+++ api_documentation.py
@@ -1,3 +1,4 @@
+base_url = "https://wallzy.onrender.com"
 api_docs = {
     "API Documentation": {
         "title": "Wallzy - A Curated Wallpaper API",
@@ -5,44 +6,44 @@ api_docs = {
         "endpoints": {
             "/random": {
                 "description": "Returns a random image from the collection, could belong to any category.",
-                "url": "/random"
+                "url": f"{base_url}/random"
             },
             "/nature": {
                 "description": "Images featuring natural elements like forests, mountains, lakes, flowers etc. Present in a variety of art-styles.",
-                "url": "/nature",
+                "url": f"{base_url}/nature",
             },
             "/ghibli": {
                 "description": "Stills captured directly from Studio Ghibli's beautiful movies.",
-                "url": "/ghibli",
+                "url": f"{base_url}/ghibli",
             },
             "/city": {
                 "description": "Images including bustling cities, quaint towns, buildings, bridges and other architecture.",
-                "url": "/city",
+                "url": f"{base_url}/city",
             },
             "/abstract": {
                 "description": "Patterns, shapes & colors; experimental and abstract art.",
-                "url": "/abstract",
+                "url": f"{base_url}/abstract",
             },
             "/painting": {
                 "description": "Beautiful paintings done in a variety of art forms.",
-                "url": "/painting",
+                "url": f"{base_url}/painting",
             },
             "/digital_art": {
                 "description": "Breathtaking digital artwork, created using tools like Krita, Blender, Photoshop, etc",
-                "url": "/digital_art",
+                "url": f"{base_url}/digital_art",
             },
             "/comfy": {
                 "description": "Images that evoke a cozy, 'comfy' feeling. Very subjective.",
-                "url": "/comfy",                
+                "url": f"{base_url}/comfy",                
             },
             "/best": {
                 "description": "My personal favorite images out of the entire collection.",
-                "url": "/best",
+                "url": f"{base_url}/best",
             },
             "/intersection/": {
                 "description": "Retrieve images based on multiple tags. Only the images matching all of the supplied tags will be considered.",
-                "url": "/intersection/?tags=t1&tags=t2",
-                "example_usage": "/intersection/?tags=nature&tags=painting&tags=best"
+                "url": f"{base_url}/intersection/?tags=t1&tags=t2",
+                "example_usage": f"{base_url}/intersection/?tags=nature&tags=painting&tags=best"
             }    
         }
     }
